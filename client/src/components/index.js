82686df3ea97f749fa2c372e1cfbe3e1eca28c23
Panagiotis Telonis <panagiotis.telonis@sgdigital.com>
2020-06-02 18:25:59 +0300

    revert setup proxy, remove unused files

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,4 @@
-import CreateContact from "./createContact";
-import EditContact from "./editContact";
 import ContactsList from "./contactsList";
 import ContactsForm from "./contactsForm";
 
-export { CreateContact, EditContact, ContactsList, ContactsForm };
+export { ContactsList, ContactsForm };
